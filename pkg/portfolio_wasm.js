88b4e36b3b2c2571bb289735d9c97fe10092ab0e
Bruno <bruno.wilson.m@gmail.com>
2026-05-19 19:00:30 -0300

    ajuste

--- pkg/portfolio_wasm.js
+++ pkg/portfolio_wasm.js
@@ -296,7 +296,7 @@ function __wbg_get_imports() {
             return ret;
         },
         __wbindgen_cast_0000000000000002: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [NamedExternref("MouseEvent")], shim_idx: 11, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [NamedExternref("MouseEvent")], shim_idx: 36, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm_bindgen__convert__closures_____invoke__ha604d9ca89f4e15f);
             return ret;
         },
